d0aef497afbbe87c7b4ef0eafb52eab5839e8c55
Sriram19g <gsriram200@gmail.com>
2024-12-11 16:50:18 +0530

    [SUCCESS-4] able to store the permanent mac for revert purpose

--- macspoofer.c
+++ macspoofer.c
@@ -9,11 +9,11 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <unistd.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 typedef unsigned char int8;
-const char *my_path="/home/cyberhunter/Desktop/MacSpoofer";
+const char *my_path = "/home/cyberhunter/Desktop/MacSpoofer";
 
 unsigned char revert[6];
 struct s_mac {
@@ -26,12 +26,39 @@ void parse_oui(const char *oui, unsigned char addr[6]) {
   sscanf(oui, "%2hhx:%2hhx:%2hhx", &addr[0], &addr[1], &addr[2]);
 }
 
-void set_p_mac(const char *interface){
+void set_p_mac(const char *interface) {
   char file[256];
-  sprintf(file,"%s/p_mac/%s.txt",my_path,interface); 
+  char a[18];
+  sprintf(file, "%s/p_mac/%s.txt", my_path, interface);
   struct stat buffer;
-  if(stat(file,&buffer)==0) return;
+  if (stat(file, &buffer) == 0)
+    return;
+  FILE *mp,*store;
+  char mpath[1024];
+
+  snprintf(mpath, sizeof(mpath), "/sys/class/net/%s/address", interface);
+  mp = fopen(mpath, "r");
+  if (mp == NULL) {
+    perror("Unable to open the class file");
+    exit(EXIT_FAILURE);
+  }
+  if (fgets(a, 18, mp) == NULL) {
+    perror("Error while reading MAC address");
+    exit(EXIT_FAILURE);
+  }
+  a[strcspn(a, "\n")] = '\0';
+  sscanf(a, "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx", &revert[0], &revert[1], &revert[2], &revert[3], &revert[4], &revert[5]);
   
+  store=fopen(file,"w");
+  if(store==NULL){
+    perror("Unable to open file for writing");
+    exit(EXIT_FAILURE);
+  }
+
+  fprintf(store,"%s",a);
+  fclose(mp);
+  fclose(store);
+  return;
 }
 
 Mac generatemac(const char *filename) {
@@ -86,7 +113,7 @@ bool chmac(const int8 *If, Mac mac) {
   struct ifreq ir;
   int fd, ret;
 
-  set_p_mac((const char*)If);
+  set_p_mac((const char *)If);
 
   fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
@@ -95,12 +122,6 @@ bool chmac(const int8 *If, Mac mac) {
   }
   strncpy(ir.ifr_name, (const char *)If, (IFNAMSIZ - 1));
   ir.ifr_hwaddr.sa_family = ARPHRD_ETHER;
-  printf("Old MAC address : %02x:%02x:%02x:%02x:%02x:%02x\n",
-         ir.ifr_hwaddr.sa_data[0], ir.ifr_hwaddr.sa_data[1],
-         ir.ifr_hwaddr.sa_data[2], ir.ifr_hwaddr.sa_data[3],
-         ir.ifr_hwaddr.sa_data[4], ir.ifr_hwaddr.sa_data[5]);
-
-  memcpy(revert, ir.ifr_hwaddr.sa_data, 6);
   memcpy(ir.ifr_hwaddr.sa_data, mac.addr, 6);
 
   ret = ioctl(fd, SIOCSIFHWADDR, &ir);
@@ -140,8 +161,9 @@ int main(int argc, char *argv[]) {
     printf("Permanent MAC address : %02x:%02x:%02x:%02x:%02x:%02x\n", revert[0],
            revert[1], revert[2], revert[3], revert[4], revert[5]);
 
-    printf("New MAC address       : %02x:%02x:%02x:%02x:%02x:%02x\n", ad.addr[0],
-           ad.addr[1], ad.addr[2], ad.addr[3], ad.addr[4], ad.addr[5]);
+    printf("New MAC address       : %02x:%02x:%02x:%02x:%02x:%02x\n",
+           ad.addr[0], ad.addr[1], ad.addr[2], ad.addr[3], ad.addr[4],
+           ad.addr[5]);
   } else {
     fprintf(stderr, "Failed to spoof MAC address\n");
   }
